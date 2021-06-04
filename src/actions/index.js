09085bd193351ee5f3e5b8d76eb083c8961d22b5
iamsaleh <salehfarag161@gmail.com>
2021-06-05 01:05:19 +0200

    fixed  the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
 import {FETCH_WEATHER} from './types'
 import axios from 'axios'
 const API_KEY ='80f6beefadb22586e031a05ab96c3c7e';
-const proxy = 'https://cors-anywhere.herokuapp.com/';
-const ROOT_URL = `${proxy}https:api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const fetchWeather = (city) => {
     const url = `${ROOT_URL}&q=${city}`;
